3309e4234c4c9960673189d2c144f3298c91839b
carmenvegga <maricarmenvegaislas@gmail.com>
2017-10-30 09:16:09 -0600

    probando subir dos carpetas

---
+++ pistas_finalsprint1/README.md
@@ -0,0 +1,5 @@
+## Pistas sobre proyecto final
+1. repaso sobre métodos y arreglos
+2. métodos para los strings
+3. Ascii
+4. for, condicionales (if, if else)
---
+++ pistas_finalsprint1/js/app.js
@@ -0,0 +1,26 @@
+var testArray = [];
+for (var i = 0; i <= 10; i ++){
+//agregar un nuevo elemento al final del array
+testArray.push(i);
+}
+console.log(testArray);
+
+//eliminar el primer elemento del arreglo
+  testArray.shift();
+console.log(testArray);
+
+//eliminar el último elemento del array
+  testArray.pop();
+console.log(testArray);
+
+//invertir los elementos del arreglo
+  testArray.reverse();
+console.log(testArray);
+
+var testString = "abcde"
+ testString.charAt(2); */
+/*Para obtener un caracter a partir de su código Ascii, utilizo el método
+string.fromCharcode */
+/* var codigo = testString.charCodeAt(2);
+console.log(codigo);
+*/
